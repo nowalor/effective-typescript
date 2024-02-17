07a21bcca6f226464ef868a9ff6620e0164d5c4a
nowalor <no@walor.io>
2024-02-17 10:38:57 +0100

    no message

--- item15.ts
+++ item15.ts
@@ -30,3 +30,40 @@ const falconHeavy: RockerInterface = {
   variant: "v1",
   thrust: 15_200,
 };
+
+/* ------------------------------------- */
+
+/*
+ * An example where you would actually use an index signature
+ * When the exact input and output is not clear
+ * Like in a CSV parser function
+ */
+interface CSVRow {
+  [columnName: string]: string;
+}
+
+function parseCSV(input: string): CSVRow[] {
+  const lines = input.split("\n");
+  const [headerLine, ...rows] = lines; // Name,Age,City, [ 'John,25,New York', 'Alice,30,Los Angeles', 'Bob,22,Chicago' ]
+  console.log(headerLine);
+
+  const headers = headerLine.split(","); // [ 'Name', 'Age', 'City' ]
+
+  return rows.map((rowStr /* 'John,25,New York' */) => {
+    const row: CSVRow = {};
+
+    rowStr.split(",").forEach((cell, i) => {
+      row[headers[i]] = cell; // row['Name' | 'Age' | 'City'] = 'John' | '25', 'NewYork'
+    });
+
+    return row;
+  });
+}
+
+const example1CSV = `Name,Age,City
+John,25,New York
+Alice,30,Los Angeles
+Bob,22,Chicago`;
+
+const parsedCSV = parseCSV(example1CSV);
+console.log(parsedCSV);
