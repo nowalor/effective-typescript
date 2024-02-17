09d40f58f4e0b56020ed356257bc01e81148e91b
nowalor <no@walor.io>
2024-02-17 09:52:22 +0100

    add some notes on item15 example1

--- item15.ts
+++ item15.ts
@@ -1,6 +1,8 @@
 /*
  * Name of the key "property" is purely for documentation
  * Key names are not used by type checker in any way
+ * This will allow any key for "Rocket"(probably not optimal)
+ * "{}" is technically a valid "Rocket"
  */
 type Rocket = { [property: string]: string };
 
@@ -12,3 +14,5 @@ const rocket: Rocket = {
   variant: "Block 5",
   thrust: "4,940 kN",
 };
+
+const emptyRocket: Rocket = {}; // ok
