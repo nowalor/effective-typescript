ead984a14d3a8108e0b621f8a53fa2b1d1552763
nowalor <no@walor.io>
2024-03-01 09:43:35 +0100

    interesting example of omit & partial together

--- item29.ts
+++ item29.ts
@@ -7,4 +7,14 @@ type LngLatLike = LngLat | { lon: number; lat: number } | [number, number];
 
 interface Camera {
   center: LngLat;
+  zoom: number;
+  bearing: number;
+  pitch: number;
+}
+
+/*
+ * Best way to make one field in a big interface optional?
+ */
+interface CameraOptions extends Omit<Partial<Camera>, "center"> {
+  center?: LngLat;
 }
