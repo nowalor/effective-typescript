28cddf6be867224299461a1ae9fa5b0e6ee55577
nowalor <no@walor.io>
2024-03-01 09:49:56 +0100

    no message

--- item29.ts
+++ item29.ts
@@ -16,10 +16,17 @@ interface Camera {
  * Best way to make one field in a big interface optional?
  */
 interface CameraOptions extends Omit<Partial<Camera>, "center"> {
-  center?: LngLat;
+  center?: LngLatLike;
 }
 
 type LngLagBounds =
   | { northeast: LngLatLike; southwest: LngLatLike }
   | [LngLatLike, LngLatLike]
   | [number, number, number, number];
+
+/*
+ * Examples of two functions that are very implicit in what they take in and strict in what they produce
+ */
+
+declare function setCamera(camera: CameraOptions): void;
+declare function viewportForBounds(bounds: LngLagBounds): Camera;
